915cda23eba9435e954043ee5fb87674d33e8b2c
Marcelino <marcelino85albuquerque@gmail.com>
2024-01-10 10:07:25 -0300

    Criando a tela de logado

--- src/Pages/feed/index.jsx
+++ src/Pages/feed/index.jsx
@@ -1,15 +1,33 @@
 import { Link } from 'react-router-dom'
 import bannerImagem from '../../assets/banner.png'
 import { Button } from '../../components/Button'
-import { Header } from '../../components/Header'
-import { Container, TextContent, Title, TitleHighlight } from './styles'
 import { Card } from '../../components/Cards'
-const Feed = ()=>{
+import { UserInfo } from '../../components/UserInfo'
+import { Header } from '../../components/Header'
+import { Container, column ,Title, TitleHighlight, Column } from './styles'
+
+const Feed = () => {
     return(
         <>
         <Header />
+       
         <Container>
-            <Card />
+            <Column flex={3}>
+                <Card />
+                
+            </Column>
+
+            <Column flex={1}>
+            <TitleHighlight># TOP 5 DA SEMANA</TitleHighlight>
+          <UserInfo
+            nome="Marcelino Albuquerque"
+            image="https://avatars.githubusercontent.com/u/106674529?v=4"
+            percentual={40}
+          />
+                          
+               
+            </Column>
+            
         </Container>
         
         </>
--- src/Pages/feed/styles.js
+++ src/Pages/feed/styles.js
@@ -1,39 +1,37 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.main`
-    width:100%;
-    max-width:80%;
-    margin:0 auto;
-    margin-top:120px;
+    width: 100%;
+    max-width: 80%;
+    margin: 0 auto;
+    margin-top: 120px;
 
-    display:flex; 
-    flex-direction:row;
-    justify-content:space-between;
-    align-items:center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `
-export const Title = styled.h2`
-    font-family:"Open Sans";
-    font-style:normal;
-    font-weight:700;
-    font-size:32px;
-    width:320px;
-    margin-bottom:20px;
-    line-height:44px;
 
-    color:#FFFFFF;
+export const Title = styled.h3`
+    font-family: 'Open Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    color: #ffffff;
+    margin-bottom: 24px;
 `
-export const TitleHighlight = styled.span`
 
-    color:#E41055;
+export const TitleHighlight = styled.h3`
+    font-family: 'Open Sans', sans-serif;
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    color: #ffffff70;
+    margin-bottom: 24px;
 `
-export const TextContent = styled.p`
-font-family:"Open Sans";
-font-style:normal;
-font-weight:400;
-font-size:16px;
-width:420px;
-margin-bottom:20px;
-line-height:22px;
 
-color:#FFFFFF;
+export const Column = styled.div`
+    flex:${({flex}) => flex};
+    padding-right: 24px;
 `
